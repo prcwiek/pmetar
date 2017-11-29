2bb4bf5b1de3602dfd03740f4be92cc41e322cc0
prcwiek <altodato@outlook.com>
2017-11-29 14:51:00 +0100

    Adding help

--- R/metar_speed.R
+++ R/metar_speed.R
@@ -1,11 +1,18 @@
-#' metar_speed
+#' Extract wind speed
 #'
-#' @param x
+#' Function extract a wind speed value from METAR weather report.
 #'
-#' @return
+#' @param x Input character vector
+#'
+#' @return A numeric vector. A wind speed in m/s.
+#'
 #' @export
 #'
 #' @examples
+#' #' metar_speed(""EPWA 281830Z 18009KT 140V200 9999 SCT037 03/M01 Q1008 NOSIG"")
+#' metar_speed("CYUL 281800Z 13008KT 30SM BKN240 01/M06 A3005 RMK CI5 SLP180")
+#' metar_speed("201711271930 METAR LEMD 271930Z 02002KT CAVOK 04/M03 Q1025 NOSIG= NOSIG=")
+#'
 metar_speed <- function(x){
   if(str_detect(x, pattern = one_or_more(DGT) %R% "MPS")) {
     speed <- str_extract(x, pattern = one_or_more(DGT) %R% "MPS")
