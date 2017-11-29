1798dde1c3fd0ccdf0fce4f2addfa87d5c238e55
prcwiek <altodato@outlook.com>
2017-11-29 15:42:15 +0100

    Adding help. Correction of airport IATA code.

--- R/metar_location.R
+++ R/metar_location.R
@@ -1,24 +1,38 @@
-#' metar_location
+#' Extract approximated airport location.
 #'
-#' @param x
+#' Function extract approximated airport lattitude, longitude and elevation for a given airport code.
 #'
-#' @return
+#' @param x Input character vector
+#'
+#' @return A data frame with three columns longitude, latitude, elevation in meters and one row.
+#'
 #' @export
 #'
 #' @examples
+#' metar_location("EPWA")
+#' metar_location("CYUL")
+#' metar_location("LEMD")
+#'
 metar_location <- function(x) {
   if(nchar(x) == 0){
     cat("Airport not specified!\n")
-  } else if (sum(str_count(mst, pattern = x %R% one_or_more(SPC) %R% one_or_more(DGT) %R% one_or_more(SPC)
-                           %R% one_or_more(DGT) %R% SPC %R% one_or_more(DGT) %R% or("N", "S")
-                           %R% one_or_more(SPC) %R% one_or_more(DGT) %R% SPC %R% one_or_more(DGT)
-                           %R% or("E", "W") %R% one_or_more(SPC) %R% one_or_more(DGT))) < 1) {
+  } else if (sum(str_count(mst, pattern = x)) < 1) {
     cat(paste("Airport ", x, " not found!\n", sep = ""))
   } else {
-    m_t <- str_extract(mst, pattern = x %R% one_or_more(SPC) %R% one_or_more(DGT) %R% one_or_more(SPC)
+    if(sum(str_count(mst, pattern = x %R% one_or_more(SPC) %R% one_or_more(DGT) %R% one_or_more(SPC)
+                     %R% one_or_more(DGT) %R% SPC %R% one_or_more(DGT) %R% or("N", "S")
+                     %R% one_or_more(SPC) %R% one_or_more(DGT) %R% SPC %R% one_or_more(DGT)
+                     %R% or("E", "W") %R% one_or_more(SPC) %R% one_or_more(DGT))) == 1) {
+      m_t <- str_extract(mst, pattern = x %R% one_or_more(SPC) %R% one_or_more(DGT) %R% one_or_more(SPC)
                        %R% one_or_more(DGT) %R% SPC %R% one_or_more(DGT) %R% or("N", "S")
                        %R% one_or_more(SPC) %R% one_or_more(DGT) %R% SPC %R% one_or_more(DGT)
                        %R% or("E", "W") %R% one_or_more(SPC) %R% one_or_more(DGT))
+    } else {
+      m_t <- str_extract(mst, pattern = x %R% one_or_more(SPC) %R% one_or_more(WRD) %R% one_or_more(SPC) %R% one_or_more(DGT) %R% one_or_more(SPC)
+                         %R% one_or_more(DGT) %R% SPC %R% one_or_more(DGT) %R% or("N", "S")
+                         %R% one_or_more(SPC) %R% one_or_more(DGT) %R% SPC %R% one_or_more(DGT)
+                         %R% or("E", "W") %R% one_or_more(SPC) %R% one_or_more(DGT))
+    }
     m_t <- m_t[!is.na(m_t)]
     lat <- str_extract(m_t, pattern = one_or_more(DGT) %R% SPC %R% one_or_more(DGT) %R% or("N", "S"))
     lat <- str_sub(lat, 1, nchar(lat) - 1)
@@ -27,7 +41,6 @@ metar_location <- function(x) {
     lon <- str_sub(lon, 1, nchar(lon) - 1)
     lon <- as.numeric(str_replace(lon, " ", "."))
     ele <- as.numeric(str_extract(m_t, pattern = one_or_more(DGT) %R% END))
-
     data.frame(longitude = lon, latitude = lat, elevation = ele, stringsAsFactors = FALSE)
   }
 }
