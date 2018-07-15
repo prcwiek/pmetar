498a9fadbf15165c41f8ffc6bc50d95179bb25c2
prcwiek <altodato@outlook.com>
2018-07-15 21:16:27 +0200

    simplification

--- R/metar_dir.R
+++ R/metar_dir.R
@@ -14,26 +14,14 @@
 #' metar_dir("201711271930 METAR LEMD 271930Z 02002KT CAVOK 04/M03 Q1025 NOSIG= NOSIG=")
 #'
 metar_dir <- function(x){
+  oldwarn <- getOption("warn")
+  options(warn = -1)
   dirw <- c(1:length(x))
   dirw[1:length(x)] <- -1
-  fDIR <- str_detect(x, pattern = "[\\d]+G[\\d]+KT")
-  dirw[fDIR] <- as.numeric(str_sub(str_extract(x[fDIR], pattern = "[\\d]+G[\\d]+KT"), 1, 3))
+  fDIR <- str_detect(x, pattern = "([\\d]+G[\\d]+KT|[\\d]+KT|[\\d]+MPS)")
+  dirw[fDIR] <- as.numeric(str_sub(str_extract(x[fDIR], pattern = "([\\d]+G[\\d]+KT|[\\d]+KT|[\\d]+MPS)"), 1, 3))
   fDIR <- str_detect(x, pattern = "VRB[\\d]+KT")
   dirw[fDIR] <- NA
-  fDIR <- str_detect(x, pattern = "[\\d]+KT")
-  dirw[fDIR] <- as.numeric(str_sub(str_extract(x[fDIR], pattern = "\\d\\d\\d\\d\\dKT"), 1, 3))
-  fDIR <- str_detect(x, pattern = "[\\d]+MPS")
-  dirw[fDIR] <- as.numeric(str_sub(str_extract(x[fDIR], pattern = "\\d\\d\\d\\d\\dMPS"), 1, 3))
+  options(warn = oldwarn)
   dirw
-    #
-  # if(str_detect(x, pattern = "[\\d]+G[\\d]+KT")){
-  #   dirw <- str_extract(x, pattern = "[\\d]+G[\\d]+KT")
-  #   as.numeric(str_sub(dirw, 1, 3))
-  # } else if(str_detect(x, pattern = "VRB[\\d]+KT")){
-  #   dirw <- NA
-  #   dirw
-  # } else {
-  #   dirw <- str_extract(x, pattern = "[\\d]+KT")
-  #   as.numeric(str_sub(dirw, 1, 3))
-  # }
 }
