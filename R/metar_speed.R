1af2bba8e74ab6d605522d221f6828bfe09861e8
prcwiek <altodato@outlook.com>
2018-07-21 15:59:22 +0200

    initial value to zero, removing redundant lines

--- R/metar_speed.R
+++ R/metar_speed.R
@@ -14,17 +14,11 @@
 #' metar_speed("201711271930 METAR LEMD 271930Z 02002KT CAVOK 04/M03 Q1025 NOSIG= NOSIG=")
 #'
 metar_speed <- function(x){
+  speed <- c(1:length(x))
+  speed[c(1:length(x))] <- 0
   fMPS <- str_detect(x, pattern = "\\d\\dMPS")
   fKT <- str_detect(x, pattern = "\\d\\dKT")
-  speed <- c(1:length(x))
   speed[fMPS] <- as.numeric(str_sub(str_extract(x[fMPS], pattern = "\\d\\dMPS"), 1, 2))
   speed[fKT] <- as.numeric(str_sub(str_extract(x[fKT], pattern = "\\d\\dKT"), 1, 2)) * 0.514444
   speed
-  # if(str_detect(x, pattern = "[\\d]+MPS")) {
-  #   speed <- str_extract(x, pattern = "[\\d]+MPS")
-  #   as.numeric(str_sub(speed, 1, 2))
-  # } else if(str_detect(x, pattern = "[\\d]+KT")) {
-  #   speed <- str_extract(x, pattern = "[\\d]+KT")
-  #   as.numeric(str_sub(speed, 1, 2)) * 0.514444
-  # }
 }
