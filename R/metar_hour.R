5d821f3fe9ab30bdfe0441e76f9d18b52ef96dc8
prcwiek <altodato@outlook.com>
2018-09-15 17:19:44 +0200

    simplifying

--- R/metar_hour.R
+++ R/metar_hour.R
@@ -14,6 +14,6 @@
 #' metar_hour("201711271930 METAR LEMD 271930Z 02002KT CAVOK 04/M03 Q1025 NOSIG= NOSIG=")
 #'
 metar_hour <- function(x){
-  mt <- str_extract(x, pattern = "[\\w]+\\s\\d\\d\\d\\d\\d\\d.\\s")
+  mt <- str_extract(x, pattern = "[\\w]+\\s\\d{6}.\\s")
   paste(str_sub(mt, 8, 9), str_sub(mt, 10, 11), sep = ":")
 }
