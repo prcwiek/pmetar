4a73363be74cf0a6045042c07bd147840f1e8530
prcwiek <altodato@outlook.com>
2020-06-11 22:08:17 +0200

    issue #2 solution

--- R/metar_visibility.R
+++ R/metar_visibility.R
@@ -19,12 +19,17 @@ metar_visibility <- function(x) {
   fT <- str_detect(x, pattern = "\\s[\\d]+\\s")
   outvis[fT] <- as.numeric(str_sub(str_extract(x[fT], pattern = "\\s[\\d]+\\s"), 2, 5))
   fT <- str_detect(x, pattern = "\\s[\\d]+SM\\s")
-  outvis[fT] <- as.numeric(str_sub(str_extract(x[fT], pattern = "\\s[\\d]+SM\\s"), 2, 3)) * 1609.344
+  outvis[fT] <- as.numeric(str_sub(str_extract(x[fT], pattern = "\\s[\\d]+SM\\s"), 1, -4)) * 1609.344
   # CAVOK - Ceiling And Visibility OK, indicating no cloud below 5,000 ft (1,500 m) or
   # the highest minimum sector altitude and no cumulonimbus or towering cumulus at any level,
   # a visibility of 10 km (6 mi) or more and no significant weather change
   fT <- str_detect(x, pattern = "CAVOK")
   outvis[fT] <- "Ceiling And Visibility OK"
+  # cases like 1 3/4SM
+  fT <- str_detect(x, pattern = "\\s\\d\\s\\d\\/\\dSM\\s")
+  outvis[fT] <- (as.numeric(str_extract(str_extract(x[fT], pattern = "\\s\\d\\s\\d\\/\\dSM\\s"), pattern = "\\s\\d\\s")) +
+    as.numeric(str_sub(str_extract(str_extract(x[fT], pattern = "\\s\\d\\s\\d\\/\\dSM\\s"), pattern = "\\d\\/"), 1, 1)) /
+    as.numeric(str_sub(str_extract(str_extract(x[fT], pattern = "\\s\\d\\s\\d\\/\\dSM\\s"), pattern = "\\/\\d"), -1, -1))) * 1609.344
   outvis
 }
 
