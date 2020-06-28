e7382f3ef6e656bf89772c6cb970ed02f62379f1
Pawel Cwiek <altodato@outlook.com>
2020-06-28 17:38:18 +0200

    pattern change

--- R/metar_dew_point.R
+++ R/metar_dew_point.R
@@ -12,14 +12,15 @@
 #' metar_dew_point("EPWA 281830Z 18009KT 140V200 9999 SCT037 03/M01 Q1008 NOSIG")
 #' metar_dew_point("CYUL 281800Z 13008KT 30SM BKN240 01/M06 A3005 RMK CI5 SLP180")
 #' metar_dew_point("201711271930 METAR LEMD 271930Z 02002KT CAVOK 04/M03 Q1025 NOSIG= NOSIG=")
+#' metar_temp("202001010851 METAR KEWR 010851Z 27010KT 10SM FEW030 BKN070 BKN100 BKN210 04/M03 A2969 RMK SLP054 T00391033 52012")
 #'
 metar_dew_point <- function(x) {
   out <- c(1:length(x))
   out[1:length(x)] <- NA
   fT <- str_detect(x, pattern = "/M[\\d]+\\s")
-  out[fT] <- as.numeric(str_sub(str_extract(x[fT], pattern = "/M[\\d]+\\s"), 3, 4)) * -1
+  out[fT] <- as.numeric(str_sub(str_extract(x[fT], pattern = "/M\\d{2}\\s"), 3, 4)) * -1
   fT <- str_detect(x, pattern = "/[\\d]+\\s")
-  out[fT] <- as.numeric(str_sub(str_extract(x[fT], pattern = "/[\\d]+\\s"), 2, 3))
+  out[fT] <- as.numeric(str_sub(str_extract(x[fT], pattern = "/\\d{2}\\s"), 2, 3))
   fT <- str_detect(x, pattern = "T\\d{4}0\\d{3}")
   out[fT] <- as.numeric(str_sub(str_extract(x[fT], pattern = "T\\d{4}0\\d{3}"), 7, 9)) / 10.0
   fT <- str_detect(x, pattern = "T\\d{4}1\\d{3}")
--- R/metar_temp.R
+++ R/metar_temp.R
@@ -12,14 +12,15 @@
 #' metar_temp("EPWA 281830Z 18009KT 140V200 9999 SCT037 03/M01 Q1008 NOSIG")
 #' metar_temp("CYUL 281800Z 13008KT 30SM BKN240 01/M06 A3005 RMK CI5 SLP180")
 #' metar_temp("201711271930 METAR LEMD 271930Z 02002KT CAVOK 04/M03 Q1025 NOSIG= NOSIG=")
+#' metar_temp("202001010851 METAR KEWR 010851Z 27010KT 10SM FEW030 BKN070 BKN100 BKN210 04/M03 A2969 RMK SLP054 T00391033 52012")
 #'
 metar_temp <- function(x){
   out <- c(1:length(x))
   out[1:length(x)] <- NA
-  fT <- str_detect(x, pattern = "M[\\d]+/")
-  out[fT] <- as.numeric(str_sub(str_extract(x[fT], pattern = "M[\\d]+/"), 2, 3)) * -1
-  fT <- str_detect(x, pattern = "\\s[\\d]+/(?!\\dSM)")
-  out[fT] <- as.numeric(str_sub(str_extract(x[fT], pattern = "\\s[\\d]+/(?!\\dSM)"), 2, 3))
+  fT <- str_detect(x, pattern = "(M\\d{2}/\\d{2}|M\\d{2}/M\\d{2})")
+  out[fT] <- as.numeric(str_sub(str_extract(x[fT], pattern = "(M\\d{2}/\\d{2}|M\\d{2}/M\\d{2})"), 2, 3)) * -1
+  fT <- str_detect(x, pattern = "(\\d{2}/\\d{2}|\\d{2}/M\\d{2})")
+  out[fT] <- as.numeric(str_sub(str_extract(x[fT], pattern = "(\\d{2}/\\d{2}|\\d{2}/M\\d{2})"), 1, 2))
   fT <- str_detect(x, pattern = "T0[\\d]{3}")
   out[fT] <- as.numeric(str_sub(str_extract(x[fT], pattern = "T0[\\d]{3}"), 3, 5)) / 10.0
   fT <- str_detect(x, pattern = "T1[\\d]{3}")
