a5cc5f56f648ffcc53a28641a5f2355aa657fa7e
Pawel Cwiek <altodato@outlook.com>
2020-07-04 21:19:22 +0200

    rewriting code

--- R/metar_airport.R
+++ R/metar_airport.R
@@ -2,7 +2,7 @@
 #'
 #' Extracts an airport ICAO code from METAR weather report.
 #'
-#' @param x character; a METAR weather report or reports.
+#' @param x character; METAR weather report or reports.
 #'
 #' @return A character vector with an airport ICAO code.
 #'
@@ -14,11 +14,11 @@
 #' metar_airport("201711271930 METAR LEMD 271930Z 02002KT CAVOK 04/M03 Q1025 NOSIG= NOSIG=")
 #'
 metar_airport <- function(x) {
-  if(sum(stringr::str_detect(x, pattern = "^[\\d]+(?: METAR| SPECI)")) > 0){
-    out <- stringr::str_extract(x, pattern = "(?:METAR |SPECI )[\\w]+")
-    stringr::str_sub(out, nchar(out)-3, nchar(out))
-  } else {
-    out <- stringr::str_extract(x, pattern = "^[\\w]+\\s")
-    stringr::str_sub(out, 1, 4)
-  }
+  out <- c(1:length(x))
+  out[1:length(x)] <- NA
+  fT <- stringr::str_detect(x, pattern = "(^[\\d]+(?: METAR| SPECI)|^METAR|^SPECI)")
+  out[fT] <- stringr::str_sub(stringr::str_extract(x[fT], pattern = "(?:METAR|SPECI)\\s\\w{4}"), -4, -1)
+  fT <- stringr::str_detect(x, pattern = "^\\w{4}\\s")
+  out[fT] <- stringr::str_sub(stringr::str_extract(x[fT], pattern = "^\\w{4}\\s"), 1, 4)
+  out
 }
