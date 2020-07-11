158a6d7781388215db8829bd928cde31366e5dfd
Pawel Cwiek <altodato@outlook.com>
2020-07-11 19:56:20 +0200

    algorithm changed after test_that

--- R/metar_get.R
+++ R/metar_get.R
@@ -6,7 +6,7 @@
 #' the web page https://aviationweather.gov/metar/\cr
 #' \cr
 #'
-#' @param airport character; ICAO or an IATA airport code.
+#' @param x character; ICAO or an IATA airport code.
 #'
 #' @return A character vector with a current METAR weather report.
 #'
@@ -18,24 +18,32 @@
 #' metar_get("MAD")
 #' metar_get("WAW")
 #'
-metar_get <- function(airport = "EPWA"){
-  airport <- stringr::str_to_upper(airport)
-  if(stringr::str_detect(airport, pattern = "^[A-Z]{3}$")) {
-    airport <- metar_iata_icao(airport)
-  }
-  if(stringr::str_detect(airport, pattern = "^[A-Z]{4}$")) {
-    cat("Getting information from Aviation Weather Center www.aviationweather.gov/metar\n")
-    link <- paste0("https://aviationweather.gov/metar/data?ids=",
-                   airport,
-                   "&format=raw&date=0&hours=0")
-    myfile <- RCurl::getURL(link, ssl.verifyhost = FALSE, ssl.verifypeer = FALSE)
-    metar <- stringr::str_extract(myfile, pattern = "<code>[:print:]+</code>")
-    metar <- stringr::str_replace(metar, "<code>", "")
-    metar <- stringr::str_replace(metar, "</code>", "")
-    if(stringr::str_detect(metar, pattern = "No METAR found")){
-      cat(paste("No METAR found for ", airport, "!\n", sep = ""))
-    } else stringr::str_split(metar, "<br", simplify = TRUE)[1]
+metar_get <- function(x = "EPWA"){
+  # convert data frame
+  if (is.data.frame(x)) {
+    airport <- x[,1]
   } else {
-    cat("Incorrect ICAO airport code!\n")
+    airport <- x
   }
+  out <- c(1:length(airport))
+  out[1:length(airport)] <- NA
+  # all characters to upper cases
+  airport <- stringr::str_to_upper(airport)
+  # find IATA codes
+  fT <- stringr::str_detect(airport, pattern = "^[A-Z]{3}$")
+  # convert IATA codes to ICAO codes
+  airport[fT] <- metar_iata_icao(airport[fT])
+  # find IACO codes
+  fT <- stringr::str_detect(airport, pattern = "^[A-Z]{4}$")
+  # get METAR report for each element
+  cat("Getting information from Aviation Weather Center www.aviationweather.gov/metar\n")
+  link <- paste0("https://aviationweather.gov/metar/data?ids=",
+                 airport,
+                 "&format=raw&date=0&hours=0")
+  myfile <- RCurl::getURL(link, ssl.verifyhost = FALSE, ssl.verifypeer = FALSE)
+  metar <- stringr::str_extract(myfile, pattern = "<code>[:print:]+</code>")
+  metar <- stringr::str_replace(metar, "<code>", "")
+  metar <- stringr::str_replace(metar, "</code>", "")
+  metar[is.na(metar)] <- "No METAR found!"
+  metar
 }
--- R/metar_iata_icao.R
+++ R/metar_iata_icao.R
@@ -1,7 +1,7 @@
 #' Convert IATA, International Air Transport Association, airport code to\cr
 #' ICAO, International Civil Aviation Organization,  airport code and vice versa.
 #'
-#' @param x character; an airport ICAO four letters code or an IATA three letters code.
+#' @param code character; an airport ICAO four letters code or an IATA three letters code.
 #'
 #' @return A character vector with an IATA code an ICAO input code or an ICAO code an IATA input code.
 #'
@@ -17,24 +17,25 @@
 #' metar_iata_icao("FRA")
 #' metar_iata_icao("KRK")
 #'
-metar_iata_icao <- function(x = "WAW") {
+metar_iata_icao <- function(code = "WAW") {
   cat("Getting airport informaiton from the file downloaded from\n")
   cat("http://ourairports.com/data/airports.csv\n")
-  x <- stringr::str_to_upper(x)
-  if(stringr::str_detect(x, pattern = "^[A-Z]{4}$")){
-    out <- ourairports %>%
-      dplyr::filter(ident == x) %>%
-      dplyr::select(iata_code)
-  } else if(stringr::str_detect(x, pattern = "^[A-Z]{3}$")){
-    out <- ourairports %>%
-      dplyr::filter(iata_code == x) %>%
-      dplyr::select(ident)
+  # convert data frame
+  if (is.data.frame(code)) {
+    x <- code[,1]
   } else {
-    stop("Incorrect ICAO or IATA airport code!\n", call. = FALSE)
+    x <- code
   }
-  if(nrow(out) > 0){
-    as.character(out)
-  } else{
-    cat("Airport cannot be found!\n")
-  }
+  out <- c(1:length(x))
+  out[1:length(x)] <- "Incorrect ICAO or IATA airport code!"
+  # all characters to upper cases
+  x <- stringr::str_to_upper(x)
+  # convert ICAO to IATA
+  fT <- stringr::str_detect(x, pattern = "^[A-Z]{4}$")
+  out[fT] <- ourairports$iata_code[match(x[fT], ourairports$ident)]
+  # convert IATA to ICAO
+  fT <- stringr::str_detect(x, pattern = "^[A-Z]{3}$")
+  out[fT] <- ourairports$ident[match(x[fT], ourairports$iata_code)]
+  out[is.na(out)] <- "Incorrect ICAO or IATA airport code!"
+  out
 }
