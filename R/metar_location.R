0b6e5733f5973d795cbb57a0792e2a9b06e14321
Pawel Cwiek <altodato@outlook.com>
2020-07-12 22:01:37 +0200

    changes after test_that

--- R/metar_location.R
+++ R/metar_location.R
@@ -15,9 +15,51 @@
 #' metar_location("EPWA")
 #' metar_location("CYUL")
 #' metar_location("LEMD")
+#' metar_location("NCRK")
 #' metar_loaction("WAW")
 #' metar_location("FRA")
 #'
+
+mystr_extract <- function(p){
+  m_t <- stringr::str_extract(mst, pattern = paste(p, "(?:[\\s]+[\\d]+[\\s]+|\\s\\s...[\\s]+[\\d]+[\\s]+|\\s\\s...[\\s]+)[\\d]+\\s[\\d]+(?:N|S)[\\s]+[\\d]+\\s[\\d]+(?:E|W)[\\s]+[\\d]+", sep = ""))
+  m_t <- m_t[!is.na(m_t)]
+  if(length(m_t) > 1){
+    m_t <- m_t[1]
+  }
+  # extract latitude
+  lat <- stringr::str_extract(m_t, pattern = "[\\d]+\\s[\\d]+(?:N|S)")
+  if(stringr::str_sub(lat, nchar(lat), nchar(lat)) == "N"){
+    mlat <- 1
+  } else {
+    mlat  <- -1
+  }
+  lat <- stringr::str_sub(lat, 1, nchar(lat) - 1)
+  lat <- stringr::str_split(lat, " ")
+  lat <- (as.numeric(lat[[1]][1]) + as.numeric(lat[[1]][2])/60) * mlat
+  # extract longitude
+  lon <- stringr::str_extract(m_t, pattern = "[\\d]+\\s[\\d]+(?:E|W)")
+  if(stringr::str_sub(lon, nchar(lon), nchar(lon)) == "E"){
+    mlon <- 1
+  } else {
+    mlon  <- -1
+  }
+  lon <- stringr::str_sub(lon, 1, nchar(lon) - 1)
+  lon <- stringr::str_split(lon, " ")
+  lon <- (as.numeric(lon[[1]][1]) + as.numeric(lon[[1]][2])/60) * mlon
+  # extract elevation in meters
+  ele <- as.numeric(stringr::str_extract(m_t, pattern = "[\\d]+$"))
+  # extract airport name
+  m_t <- stringr::str_extract(mst, pattern = paste("^(.*?)", p, sep = ""))
+  m_t <- m_t[!is.na(m_t)]
+  apname <- stringr::str_extract(m_t, pattern = paste("^(.*?)", p, sep = ""))
+  apname <- stringr::str_trim(apname)
+  apname <- stringr::str_split(apname, pattern = " ", simplify = TRUE)
+  apname <- apname[1,1:ncol(apname) - 1]
+  apname <- stringr::str_c(apname, collapse = " ")
+  apname <- stringr::str_trim(apname)
+  list(apname, lat, lon, ele)
+}
+
 metar_location <- function(code = "EPWA") {
   cat("Getting airport informaiton from the file downloaded from\n")
   cat("http://ourairports.com/data/airports.csv\n")
@@ -43,67 +85,26 @@ metar_location <- function(code = "EPWA") {
       Elevation = round(ourairports$elevation_m[nmatched], 4),
       Source = "http://ourairports.com/data/airports.csv"
     )
-    return(outlocation)
+    #return(outlocation)
   }
   # try to use the other source of airports locations
   # IATA code is available
-  if((sum(complete.cases(outlocation$ICAO.code)) == nrow(outlocation)) &
-      (sum(complete.cases(outlocation$IATA.code)) != nrow(outlocation))){
+  if((sum(complete.cases(outlocation$ICAO_Code)) == nrow(outlocation)) &
+     (sum(complete.cases(outlocation$IATA_Code)) != nrow(outlocation))){
     cat("Getting airport informaiton from the file downloaded from\n")
     cat("www.aviationweather.gov/docs/metar/stations.txt\n")
     cat("prepared by Greg Thompson NCAR/RAP\n")
-    mystr_extract <- function(p){
-      m_t <- stringr::str_extract(mst, pattern = paste(p, "(?:[\\s]+[\\d]+[\\s]+|\\s\\s...[\\s]+[\\d]+[\\s]+|\\s\\s...[\\s]+)[\\d]+\\s[\\d]+(?:N|S)[\\s]+[\\d]+\\s[\\d]+(?:E|W)[\\s]+[\\d]+", sep = ""))
-      m_t <- m_t[!is.na(m_t)]
-      if(length(m_t) > 1){
-        m_t <- m_t[1]
-      }
-      # extract latitude
-      lat <- stringr::str_extract(m_t, pattern = "[\\d]+\\s[\\d]+(?:N|S)")
-      if(stringr::str_sub(lat, nchar(lat), nchar(lat)) == "N"){
-        mlat <- 1
-      } else {
-        mlat  <- -1
-      }
-      lat <- stringr::str_sub(lat, 1, nchar(lat) - 1)
-      lat <- stringr::str_split(lat, " ")
-      lat <- (as.numeric(lat[[1]][1]) + as.numeric(lat[[1]][2])/60) * mlat
-      # extract longitude
-      lon <- stringr::str_extract(m_t, pattern = "[\\d]+\\s[\\d]+(?:E|W)")
-      if(stringr::str_sub(lon, nchar(lon), nchar(lon)) == "E"){
-        mlon <- 1
-      } else {
-        mlon  <- -1
-      }
-      lon <- stringr::str_sub(lon, 1, nchar(lon) - 1)
-      lon <- stringr::str_split(lon, " ")
-      lon <- (as.numeric(lon[[1]][1]) + as.numeric(lon[[1]][2])/60) * mlon
-      # extract elevation in meters
-      ele <- as.numeric(stringr::str_extract(m_t, pattern = "[\\d]+$"))
-      # extract airport name
-      m_t <- stringr::str_extract(mst, pattern = paste("^(.*?)", p, sep = ""))
-      m_t <- m_t[!is.na(m_t)]
-      apname <- stringr::str_extract(m_t, pattern = paste("^(.*?)", p, sep = ""))
-      apname <- stringr::str_trim(apname)
-      apname <- stringr::str_split(apname, pattern = " ", simplify = TRUE)
-      apname <- apname[1,1:ncol(apname) - 1]
-      apname <- stringr::str_c(apname, collapse = " ")
-      apname <- stringr::str_trim(apname)
-      list(apname, lat, lon, ele)
-    }
 
     m_l <- c(1:length(x))
     m_l[1:length(x)] <- ""
-    nmissing <- which(is.na(outlocation$IATA.code) & !is.na(outlocation$ICAO.code))
-    m_l <- sapply(x, mystringr::str_extract)
-    outlocation$IATA.code[nmissing] <- "not found"
-    outlocation$airport.name[nmissing] <- unlist(m_l[1, nmissing])
-    outlocation$longitude[nmissing] <- unlist(m_l[3, nmissing])
-    outlocation$latitude[nmissing] <- unlist(m_l[2, nmissing])
-    outlocation$elevation[nmissing] <- unlist(m_l[4, nmissing])
+    nmissing <- which(is.na(outlocation$IATA_Code) & !is.na(outlocation$ICAO_Code))
+    m_l <- sapply(x, mystr_extract)
+    outlocation$IATA_Code[nmissing] <- "Not found!"
+    outlocation$Airport_Name[nmissing] <- unlist(m_l[1, nmissing])
+    outlocation$Longitude[nmissing] <- round(unlist(m_l[3, nmissing]), 2)
+    outlocation$Latitude[nmissing] <- round(unlist(m_l[2, nmissing]), 2)
+    outlocation$Elevation[nmissing] <- round(unlist(m_l[4, nmissing]), 0)
     outlocation$Source[nmissing] <- "www.aviationweather.gov/docs/metar/stations.txt"
   }
-
   outlocation
-
 }
