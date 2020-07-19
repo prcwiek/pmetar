159751bd9131629098b7f2a1149b2ec671b05593
Pawel Cwiek <altodato@outlook.com>
2020-07-19 22:43:48 +0200

    numeric_values_only

--- R/metar_visibility.R
+++ R/metar_visibility.R
@@ -4,6 +4,7 @@
 #'
 #' @param x character vector; a METAR weather report or reports.
 #' @param metric For the default metric = TRUE returned distances are in meters. If it's FALSE, in miles.
+#' @param numeric_values_only logical; if TRUE only a numeric value will be returned
 #'
 #' @return A numeric vector with visibility information, in meters or miles.
 #'
@@ -15,7 +16,7 @@
 #' metar_visibility("201711271930 METAR LEMD 271930Z 02002KT CAVOK 04/M03 Q1025 NOSIG= NOSIG=")
 #' metar_visibility("KBLV 011657Z AUTO 25015G30KT 210V290 3/8SM R32L/1000FT FG BKN005 01/M01 A2984 RMK A02 SLP03")
 #'
-metar_visibility <- function(x, metric = TRUE) {
+metar_visibility <- function(x, metric = TRUE, numeric_values_only = FALSE) {
   # check if x is a data frame
   if(is.data.frame(x)){
     stop("Invalid input format! Argument is not an atomic vector.", call. = FALSE)
@@ -33,22 +34,27 @@ metar_visibility <- function(x, metric = TRUE) {
   out[1:length(x)] <- NA
   # cases like 1 3/4SM
   fT <- stringr::str_detect(x, pattern = "\\s\\d\\s\\d\\/\\dSM\\s")
-  out[fT] <- (as.numeric(stringr::str_extract(stringr::str_extract(x[fT], pattern = "\\s\\d\\s\\d\\/\\dSM\\s"), pattern = "\\s\\d\\s")) +
+  out[fT] <- round((as.numeric(stringr::str_extract(stringr::str_extract(x[fT], pattern = "\\s\\d\\s\\d\\/\\dSM\\s"), pattern = "\\s\\d\\s")) +
                    as.numeric(stringr::str_sub(stringr::str_extract(stringr::str_extract(x[fT], pattern = "\\s\\d\\s\\d\\/\\dSM\\s"), pattern = "\\d\\/"), 1, 1)) /
-                   as.numeric(stringr::str_sub(stringr::str_extract(stringr::str_extract(x[fT], pattern = "\\s\\d\\s\\d\\/\\dSM\\s"), pattern = "\\/\\d"), -1, -1))) * cfi
+                   as.numeric(stringr::str_sub(stringr::str_extract(stringr::str_extract(x[fT], pattern = "\\s\\d\\s\\d\\/\\dSM\\s"), pattern = "\\/\\d"), -1, -1))) * cfi,
+                   2)
   # cases like 3/4SM
   fT2 <- stringr::str_detect(x, pattern = "\\s\\d\\/\\dSM\\s")
   fT <- !(fT & fT2)
-  out[fT] <- (as.numeric(stringr::str_sub(stringr::str_extract(x[fT], pattern = "\\s\\d\\/\\dSM\\s"), 2, 2)) /
-                   as.numeric(stringr::str_sub(stringr::str_extract(x[fT], pattern = "\\s\\d\\/\\dSM\\s"), 4, 4))) * cfi
+  out[fT] <- round((as.numeric(stringr::str_sub(stringr::str_extract(x[fT], pattern = "\\s\\d\\/\\dSM\\s"), 2, 2)) /
+                   as.numeric(stringr::str_sub(stringr::str_extract(x[fT], pattern = "\\s\\d\\/\\dSM\\s"), 4, 4))) * cfi, 2)
   fT <- stringr::str_detect(x, pattern = "\\s\\d{4}\\s")
-  out[fT] <- as.numeric(stringr::str_sub(stringr::str_extract(x[fT], pattern = "\\s[\\d]{4}\\s"), 2, 5)) * cfm
+  out[fT] <- round(as.numeric(stringr::str_sub(stringr::str_extract(x[fT], pattern = "\\s\\d{4}\\s"), 2, 5)) * cfm, 2)
   fT <- stringr::str_detect(x, pattern = "\\s[\\d]+SM\\s")
-  out[fT] <- as.numeric(stringr::str_sub(stringr::str_extract(x[fT], pattern = "\\s[\\d]+SM\\s"), 1, -4)) * cfi
+  out[fT] <- round(as.numeric(stringr::str_sub(stringr::str_extract(x[fT], pattern = "\\s[\\d]+SM\\s"), 1, -4)) * cfi, 2)
   # CAVOK - Ceiling And Visibility OK, indicating no cloud below 5,000 ft (1,500 m) or
   # the highest minimum sector altitude and no cumulonimbus or towering cumulus at any level,
   # a visibility of 10 km (6 mi) or more and no significant weather change
   fT <- stringr::str_detect(x, pattern = "CAVOK")
-  out[fT] <- "Ceiling And Visibility OK"
+  if (numeric_values_only) {
+    out[fT] <- round(10000.00 * cfm, 2)
+  } else {
+    out[fT] <- "Ceiling And Visibility OK"
+  }
   out
 }
