bca97272ea5e55efb15d43b6f7c778efc6671c1d
Pawel Cwiek <altodato@outlook.com>
2020-09-01 19:07:20 +0200

    remove commented

--- tests/testthat/test_metar_get_historical.R
+++ tests/testthat/test_metar_get_historical.R
@@ -1,27 +1,5 @@
 context("METAR get historical")
 
-# test_that("Check if METAR reports for the year 2019 from EPWA are downloaded correctly", {
-#   skip_on_cran()
-#   path_data <- file.path(devtools::inst(name="pmetar"), "test_data", "EPWA_2019.RData")
-#   load(path_data)
-#   METARs_downloaded <- metar_get_historical(airport = "EPWA",
-#                                             start_date = "2019-01-01",
-#                                             end_date = "2019-12-31",
-#                                             from = "iastate")
-#   expect_equal(EPWA_2019, METARs_downloaded)
-# })
-
-# test_that("Check if METAR reports for January 2019 from FRA are downloaded correctly", {
-#   skip_on_cran()
-#   path_data <- file.path(devtools::inst(name="pmetar"), "test_data", "FRA_2019_01.RData")
-#   load(path_data)
-#   METARs_downloaded <- metar_get_historical(airport = "FRA",
-#                                             start_date = "2019-01-01",
-#                                             end_date = "2019-01-31",
-#                                             from = "ogimet")
-#   expect_equal(FRA_2019_01, METARs_downloaded)
-# })
-
 test_that("Check incorrect dates", {
   expect_error(metar_get_historical(airport = "EPWA",
                                     start_date = "2019-01-01",
