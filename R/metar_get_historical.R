cfdf37d9f1b98843838e378111c88e7437b276b3
Pawel Cwiek <altodato@outlook.com>
2021-03-03 09:43:06 +0100

    donttest changed - take too long on CRAN

--- R/metar_get_historical.R
+++ R/metar_get_historical.R
@@ -24,10 +24,10 @@
 #' @export
 #'
 #' @examples
+#' \donttest{
 #' metar_get_historical("EPWA", start_date = "2017-11-20", end_date = "2017-11-25")
 #' metar_get_historical("MAD", start_date = "2015-06-01", end_date = "2015-06-02",
 #' from = "iastate")
-#' \donttest{
 #' metar_get_historical("CYUL", start_date = "2016-07-01", end_date = "2016-07-05",
 #' from = "ogimet")
 #' }
