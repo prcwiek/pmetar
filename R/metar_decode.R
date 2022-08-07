d93005c190fc4e9b91743754ae61d2148f890065
Pawel Cwiek <altodato@outlook.com>
2022-08-07 21:51:56 +0200

    Added parameters check and sep, handling errors

--- R/metar_decode.R
+++ R/metar_decode.R
@@ -5,6 +5,7 @@
 #' Decoded METAR weather report consists of:\cr
 #' \itemize{
 #' \item Remark: Don't use for flight planning or navigation!
+#' or Incorrect METAR report! Please check the column Original_METAR.
 #' \item Airport ICAO
 #' \item Day of Month
 #' \item Hour (HH:mm)
@@ -36,8 +37,13 @@
 #' historical METAR weather reports.
 #' @param metric logical; if TRUE wind speeds returned in m/s, distances in meters.\cr
 #' If FALSE, wind speeds returned in knots and distances in miles.
-#' @param altimeter logical; if FLASE pressures returned in hPa, if TRUE in mmHg
-#' @param numeric_only logical; if TRUE only numeric values are returned
+#' @param altimeter logical; if FLASE pressures returned in hPa, if TRUE in mmHg.
+#' @param numeric_only logical; if TRUE only numeric values are returned.
+#' @param check logical; if TRUE the syntax of METAR reports will be checked and incorrect 
+#' reports will be omitted. If FALSE, the incorrect syntax of reports can cause errors and 
+#' breakdown of decoding. The default value is TRUE.
+#' @param sep character; comma or semicolon, used for separating decoded elements of weather
+#' conditions information. The default value is ";".
 #' 
 #' @return a tibble with decoded METAR weather report or reports.
 #'
@@ -54,58 +60,105 @@
 #' metar_decode("CYWG 172000Z 30015G25KT 3/4SM R36/4000FT/D -SN M05/M08 A2992")
 #' metar_decode("202103251800 METAR COR NFTL 251800Z 00000KT SCT017TCU BKN290 25/25 Q1014")
 #'
-metar_decode <- function(x, metric = TRUE, altimeter = FALSE, numeric_only = FALSE){
+metar_decode <- function(x, metric = TRUE, altimeter = FALSE,
+                         numeric_only = FALSE, check = TRUE,
+                         sep = ";"){
+  # Check sep values
+  if (!stringr::str_detect(sep, pattern = "(^;$|^,$)")) {
+    stop("pmetar package error: Invalid sep value! It must be comma or semicolon!")
+  }
+  # check syntax
+  if (check) {
+    icorrect <- metar_is_correct(x)
+    ncorrect <- metar_is_correct(x)
+  } else {
+    icorrect <- rep(TRUE, length(x))
+    ncorrect <- seq(1, length(x), 1)
+  }
+  
   tryCatch(
     expr = {
-      if(stringr::str_detect(x, pattern = "^[\\d]+ METAR")[1]) {
-        td <- c(1:length(x))
-        td <- stringr::str_extract(x, pattern = "^[\\d]+ METAR")
-        myear <- as.numeric(stringr::str_sub(td, 1, 4))
-        mmonth <- as.numeric(stringr::str_sub(td, 5, 6))
-        mday <- as.numeric(stringr::str_sub(td, 7, 8))
-        mhour <- as.numeric(stringr::str_sub(td, 9, 10))
-        mminute <- as.numeric(stringr::str_sub(td, 11, 12))
-        metar_date <- lubridate::make_datetime(myear, mmonth, mday, mhour, mminute, tz = "UTC")
+      if(sum(stringr::str_detect(x[icorrect], pattern = "^[\\d]+ METAR")) > 0) {
+        metar_date <- rep("", length(x))
+        metar_date[icorrect] <- stringr::str_extract(x[icorrect], pattern = "^[\\d]+ METAR")
+        myear <- as.numeric(stringr::str_sub(metar_date[icorrect], 1, 4))
+        mmonth <- as.numeric(stringr::str_sub(metar_date[icorrect], 5, 6))
+        mday <- as.numeric(stringr::str_sub(metar_date[icorrect], 7, 8))
+        mhour <- as.numeric(stringr::str_sub(metar_date[icorrect], 9, 10))
+        mminute <- as.numeric(stringr::str_sub(metar_date[icorrect], 11, 12))
+        metar_date[icorrect] <- as.character(lubridate::make_datetime(myear, mmonth, mday, mhour, mminute, tz = "UTC"))
+        metar_date[!icorrect] <- NA
       } else {
         metar_date <- NA
       }
-      out <- dplyr::tibble(x)
-      out <- out %>%
+      # create an empty template
+      out <- dplyr::tibble(#y = x,
+                           Remark = "Don't use for flight planning or navigation!",
+                           Airport_ICAO = NA,
+                           METAR_Date = NA,
+                           Day_of_Month = NA,
+                           Hour = NA,
+                           Time_zone = NA,
+                           Wind_speed = NA,
+                           Wind_speed_unit = ifelse(metric, "m/s", "kn"),
+                           Gust = NA,
+                           Gust_unit = ifelse(metric, "m/s", "kn"),
+                           Wind_shear = NA,
+                           Wind_direction = NA,
+                           Temperature = NA,
+                           Dew_point = NA,
+                           Pressure = NA,
+                           Pressure_unit = ifelse(!altimeter, "hPa", "mmHg"),
+                           Visibility = NA,
+                           Visibility_unit = ifelse(metric, "m", "mile"),
+                           Cloud_coverage = NA,
+                           Weather_information = NA,
+                           Runway_visibility = NA,
+                           Airport_Name = NA,
+                           Airport_IATA = NA,
+                           Longitude = NA,
+                           Latitude = NA,
+                           Elevation = NA,
+                           Decode_Date = NA,
+                           Original_METAR = x)
+      outx <- out[ncorrect, ] 
+      outx <- outx %>%
         dplyr::mutate(Remark = "Don't use for flight planning or navigation!",
-                      Airport_ICAO = metar_airport(out$x),
-                      METAR_Date = metar_date,
-                      Day_of_Month = metar_day(out$x),
-                      Hour = metar_hour(out$x),
-                      Time_zone = metar_time_zone(out$x),
-                      Wind_speed = metar_speed(out$x, metric = metric),
+                      Airport_ICAO = metar_airport(outx$Original_METAR),
+                      METAR_Date = metar_date[icorrect],
+                      Day_of_Month = metar_day(outx$Original_METAR),
+                      Hour = metar_hour(outx$Original_METAR),
+                      Time_zone = metar_time_zone(outx$Original_METAR),
+                      Wind_speed = metar_speed(outx$Original_METAR, metric = metric, check = FALSE),
                       Wind_speed_unit = ifelse(metric, "m/s", "kn"),
-                      Gust = metar_gust(out$x, metric = metric),
+                      Gust = metar_gust(outx$Original_METAR, metric = metric),
                       Gust_unit = ifelse(metric, "m/s", "kn"),
-                      Wind_shear = metar_windshear(out$x, metric = metric),
-                      Wind_direction = metar_dir(out$x, numeric_only = numeric_only),
-                      Temperature = metar_temp(out$x),
-                      Dew_point = metar_dew_point(out$x),
-                      Pressure = metar_pressure(out$x, altimeter = altimeter),
+                      Wind_shear = metar_windshear(outx$Original_METAR, metric = metric),
+                      Wind_direction = metar_dir(outx$Original_METAR, numeric_only = numeric_only),
+                      Temperature = metar_temp(outx$Original_METAR),
+                      Dew_point = metar_dew_point(outx$Original_METAR),
+                      Pressure = metar_pressure(outx$Original_METAR, altimeter = altimeter),
                       Pressure_unit = ifelse(!altimeter, "hPa", "mmHg"),
-                      Visibility = metar_visibility(out$x, metric = metric),
+                      Visibility = metar_visibility(outx$Original_METAR, metric = metric),
                       Visibility_unit = ifelse(metric, "m", "mile"),
-                      Cloud_coverage = metar_cloud_coverage(out$x),
-                      Weather_information = metar_wx_codes(out$x),
-                      Runway_visibility = metar_rwy_visibility(out$x, metric = metric))
-      apl <- metar_location(out$Airport_ICAO)
-      out <- out %>%
+                      Cloud_coverage = metar_cloud_coverage(outx$Original_METAR, sep = sep),
+                      Weather_information = metar_wx_codes(outx$Original_METAR, sep = sep),
+                      Runway_visibility = metar_rwy_visibility(outx$Original_METAR, metric = metric, sep = sep))
+      apl <- metar_location(outx$Airport_ICAO)
+      outx <- outx %>%
         dplyr::mutate(Airport_Name = apl$Airport_Name,
                       Airport_IATA = apl$IATA_Code,
                       Longitude = apl$Longitude,
                       Latitude = apl$Latitude,
                       Elevation = apl$Elevation,
-                      Decode_Date = Sys.time(),
-                      Original_METAR = out$x) %>%
-        dplyr::select(-x)
+                      Decode_Date = as.character(Sys.time()))
+      out[ncorrect,] <- outx
+      out$Remark[!ncorrect] <- "Incorrect METAR report! Please check the column Original_METAR."
       out
     },
     error = function(e){
-      stop("pmetar package error: It is not a METAR weather report!\n", call. = FALSE)
+      stop("pmetar package error: Incorrect METAR weather report found and cannot be 
+           decoded!\n", call. = FALSE)
     }
   )
 }
