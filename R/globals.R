7d3130f55da8738c998744ef324a7b65725b71e3
Pawel Cwiek <prc.altodato@gmail.com>
2026-01-14 19:27:57 +0100

    adding variables metar_reports and Ogimet data frame columns names

--- R/globals.R
+++ R/globals.R
@@ -1,3 +1,5 @@
 
 utils::globalVariables(c("metarWXcodes", "mst", "ourairports",
-                         "Type", "Abbreviation"))
+                         "Type", "Abbreviation",
+                         "ANO", "MES", "DIA", "HORA", "MINUTO", "PARTE",
+                         "metar_reports"))
